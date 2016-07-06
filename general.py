07731f549d18916f805a84f708c4f611b1d99602
XU Pengfei <gzgyxpf@gmail.com>
2016-07-07 03:53:49 +0800

    python spider

---
+++ general.py
@@ -0,0 +1,62 @@
+import os
+
+
+def create_project_dir(dir_path):
+    if not os.path.exists(dir_path):
+        print("Creating project " + dir_path)
+        os.makedirs(dir_path)
+
+
+def create_data_files(project_name, base_url):
+    queue = project_name + '/queue.txt'
+    crawled = project_name + '/crawled.txt'
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+
+def write_file(file, data):
+    f = open(file, 'w')
+    f.write(data)
+    f.close()
+
+
+def append_to_file(file, data):
+    with open(file, 'a') as f:
+        f.write(data + '\n')
+
+
+def delete_file_contents(file):
+    with open(file, 'w'):
+        pass
+
+
+def file_to_set(file_name):
+    results = set()
+    with open(file_name, 'rt') as f:
+        for line in f:
+            results.add(line.replace('\n', ''))
+    return results
+
+
+def set_to_file(urls, file):
+    delete_file_contents(file)
+    for url in sorted(urls):
+        append_to_file(file, url)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ url_finder.py
@@ -0,0 +1,23 @@
+from html.parser import HTMLParser
+from urllib import parse
+
+
+class UrlFinder(HTMLParser):
+    def __init__(self, base_url, page_url):
+        super.__init__()
+        self.base_url = base_url
+        self.page_url = page_url
+        self.urls = set()
+
+    def handle_startendtag(self, tag, attrs):
+        if tag == 'a':
+            for(attribute, value) in attrs:
+                if attribute == 'href':
+                    url = parse.urljoin(self.base_url, value)
+                    self.urls.add(url)
+
+    def page_urls(self):
+        return self.urls
+
+    def error(self, message):
+        pass
